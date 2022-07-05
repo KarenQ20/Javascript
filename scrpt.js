a065291339bcfd47fca05a83bab3af46d25a5885
karenquiroga <karenbrigith54@gmail.com>
2022-07-04 20:33:45 -0500

    se agrega funcion

--- scrpt.js
+++ scrpt.js
@@ -1,6 +1,14 @@
 //Calculador de edad
 
+console.log("Bienvenidos al calculador de edad");
+
 let numero1, numero2;
+
+function calcularEdad(numero1, numero2) {
+  resultado = numero1 - numero2;
+
+  return resultado;
+}
 
 do {
   numero1 = parseFloat(prompt("ingerse el año en que nacio"));
@@ -8,7 +16,7 @@ do {
 } while (isNaN(numero1) || isNaN(numero2));
 
 if (numero2 > numero1) {
-  alert(`su edad es: ${numero2 - numero1}`);
+  alert(`su edad es: ${calcularEdad(numero2, numero1)}`);
 } else {
   alert("usted no existe");
 }
